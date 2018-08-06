b75f0029bb2d0738115113fc615bf4b693caf25a
yanli <yanli.geo@gmail.com>
2018-08-06 16:53:03 -0500

    modify functions to work for wfdei.gpcc forcing

--- code/func_agmip_data.py
+++ code/func_agmip_data.py
@@ -1,6 +1,37 @@
-import numpy as np
 import pandas as pd
 import xarray as xr
+import statsmodels.api as sm
+import numpy as np
+
+
+def get_year_string(model_name, climate_data='agmerra'):
+    if climate_data=='agmerra':
+        s = {'cgms-wofost': [1980,2010],
+             'clm-crop': [1980,2010],
+             'epic-boku': [1980,2010],
+             'epic-iiasa': [1980,2010],
+             'gepic': [1980,2010],
+             'lpj-guess': [1980,2010],
+             'lpjml': [1980,2010],
+             'orchidee-crop': [1980,2010],
+             'papsim': [1980,2010],
+             'pdssat': [1980,2010],
+             'pegasus': [1980,2010],
+             'pepic': [1980,2010]}
+    else: # wfdei.gpcc
+        s = {'cgms-wofost': [1979,2012],
+             'clm-crop': [1980,2012],
+             'epic-boku': [1979,2010],
+             'epic-iiasa': [1979,2010],
+             'gepic': [1979,2009],
+             'lpj-guess': [1979,2009],
+             'lpjml': [1979,2010],
+             'orchidee-crop': [1979,2009],
+             'papsim': [1979,2009],
+             'pdssat': [1979,2009],
+             'pegasus': [1979,2010],
+             'pepic': [1979,2010]}
+    return s[model_name]
 
 """
 Load agmip data
@@ -9,20 +40,25 @@ model_names = ['cgms-wofost','clm-crop','epic-boku','epic-iiasa','gepic','lpj-gu
                'lpjml','orchidee-crop','papsim','pdssat','pegasus','pepic']
 anomlay = True, load the detrended anomaly data from 1981 to 2010 
 """
-def load_agmip_data(model_name, anomaly=False):
-    if anomaly:
-        f_dir = '~/Project/RMA_study/data/result/'
-        end_string = '_agmerra_hist_default_noirr_yield_mai_annual_anomaly_1981_2010.nc'
+def load_agmip_data(model_name, anomaly=False, climate_data='agmerra'):
+    year_str = get_year_string(model_name, climate_data=climate_data)
+
+    if climate_data == 'agmerra':
+       year_end=2010
     else:
-        f_dir = '~/Project/data/agmip/maize/'
-        end_string = '_agmerra_hist_default_noirr_yield_mai_annual_1980_2010.nc4'
+       year_end=2009
+
+    if anomaly:
+        f_dir = '~/Project/corn_extreme/data/result/'
+        end_string = '_%s_hist_default_noirr_yield_mai_annual_anomaly_1981_%d.nc'%(climate_data,year_end)
+    else:
+        f_dir = '~/Project/data/agmip/maize/%s/'%climate_data
+        end_string = '_%s_hist_default_noirr_yield_mai_annual_%d_%d.nc4'%(climate_data,year_str[0],year_str[1])
     
     fn = f_dir + model_name + end_string
     ds = xr.open_dataset(fn, decode_times=False)        
     return ds
 
-
-import statsmodels.api as sm
 
 """
 Calculate yield anomaly for model yield, return numpy array of the anomlay and trend term
@@ -30,7 +66,14 @@ Usage: array_ana, array_trend = get_yield_ana(ds_obs, d_model, obs_ana=False)
 array_ana, array_trend = get_yield_ana(ds_obs, [], obs_ana=True)
 when obs_ana = True, return yield_obs anamaly
 """
-def get_yield_ana(ds_obs, d, obs_ana=False):
+def get_yield_ana(ds_obs, d, obs_ana=False, climate_data='agmerra'):
+    if climate_data == 'agmerra':
+        year_end = 2010
+        n_yr = 30
+    else:
+        year_end = 2009
+        n_yr = 29 
+
     # Extract grid box where both obs and model data have values
     m1 = ds_obs['yield'].sum(axis=0).values
     
@@ -43,10 +86,10 @@ def get_yield_ana(ds_obs, d, obs_ana=False):
         mask = (m1!=0)
         idx = np.argwhere(mask)
         var_txt = 'yield'
-        d = ds_obs
+        d = ds_obs.isel(time=range(0,n_yr))
         
     # Get slope and intercept for trend
-    X = np.arange(1981,2011)
+    X = np.arange(1981,year_end+1)
     X = sm.add_constant(X)
 
     array_slope1 = np.zeros([360,720])
@@ -65,13 +108,13 @@ def get_yield_ana(ds_obs, d, obs_ana=False):
             
     
     # Get anomaly 
-    array_year = np.zeros([30,360,720])
-    for y in range(1981,2011):
+    array_year = np.zeros([n_yr,360,720])
+    for y in range(1981,year_end+1):
         array_year[y-1981:,:] = y
 
-    array_trend = np.zeros([30,360,720])
+    array_trend = np.zeros([n_yr,360,720])
 
-    for y in range(1981,2011):
+    for y in range(1981,year_end+1):
         array_trend[y-1981:,:] = array_year[y-1981:,:] * array_slope1 + array_intercept1
 
     array_ana = d[var_txt].values - array_trend
@@ -84,38 +127,53 @@ def get_yield_ana(ds_obs, d, obs_ana=False):
     return array_ana, array_trend
 
 """
-Save model yield anomaly from 1981 to 2010 for US
+Save model yield anomaly from 1981 to 2010/2009 for US
 """
-def save_agmip_anomaly():
+def save_agmip_anomaly(climate_data='agmerra'):
     ds_obs = xr.open_dataset('../data/result/corn_area_yield_1981_2010_05deg.nc')
     
     model_names = ['cgms-wofost','clm-crop','epic-boku','epic-iiasa','gepic','lpj-guess',
                   'lpjml','orchidee-crop','papsim','pdssat','pegasus','pepic']
+
+    if climate_data=='agmerra':
+        year_end = 2010
+    else:
+        year_end = 2009
+
     # saved netcdf file name
-    end_string = '_agmerra_hist_default_noirr_yield_mai_annual_anomaly_1981_2010.nc'
+    end_string = '_%s_hist_default_noirr_yield_mai_annual_anomaly_1981_%d.nc'%(climate_data,year_end)
 
     for m in model_names:
-        d = load_agmip_data(m)
+        d = load_agmip_data(m,climate_data=climate_data)
+        year_str = get_year_string(m, climate_data=climate_data) # get data year range
 
-        array_ana, array_trend = get_yield_ana(ds_obs, d.isel(time=range(1,31)))
+       # array_ana, array_trend = get_yield_ana(ds_obs, d.isel(time=range(1,31)))
+        if climate_data=='agmerra':
+            i1 = 1981 - year_str[0]
+            i2 = 31
+            array_ana, array_trend = get_yield_ana(ds_obs, d.isel(time=range(1,31)),climate_data=climate_data)
+        else: # from 1981 to 2009 for gccp
+            i1 = 1981 - year_str[0]
+            i2 = 2009 - year_str[0] + 1
+            array_ana, array_trend = get_yield_ana(ds_obs.isel(time=range(0,29)), d.isel(time=range(i1,i2)),climate_data=climate_data)
 
         # subset model data to create a new dataset
         ds = xr.Dataset({'yield_mai_ana': (['time', 'lat', 'lon'], array_ana),
                          'yield_mai_trend': (['time', 'lat', 'lon'], array_trend),
-                         'yield_mai': (['time', 'lat', 'lon'], d.isel(time=range(1,31))['yield_mai'].values)
+                         'yield_mai': (['time', 'lat', 'lon'], d.isel(time=range(i1,i2))['yield_mai'].values)
                                },
                         coords={'lon': d.lon,
                                 'lat': d.lat,
-                                'time': np.arange(1981,2011,1)
+                                'time': np.arange(1981,year_end+1,1)
                                  })
 
         ds.to_netcdf('../data/result/' + m + end_string)
-        print('%s saved'%m)
+        print('%s for %s saved'%(m,climate_data))
 
 """
 Save 0.5 degree observed corn yield anomaly and area to netcdf file
 """
-def save_yield_obs_anamaly():
+def save_yield_obs_anamaly(climate_data='agmerra'):
     ds_obs = xr.open_dataset('../data/result/corn_area_yield_1981_2010_05deg.nc')
 
     array_ana, array_trend = get_yield_ana(ds_obs, [], obs_ana=True)
@@ -144,15 +202,17 @@ def xarray2dataframe(da, name):
 """
 Load agmip model data, match with obs, prec, and tmax bin and save to csv
 """
-def agmip_to_csv():
+def agmip_to_csv(climate_data='agmerra'):
     model_names = ['obs','cgms-wofost','clm-crop','epic-boku','epic-iiasa','gepic','lpj-guess',
                   'lpjml','orchidee-crop','papsim','pdssat','pegasus','pepic']
     ds_obs = xr.open_dataset('../data/result/corn_area_yield_anomaly_1981_2010_05deg.nc')
-
+   
     ds_obs['yield_ana_to_yield'] = ds_obs['yield_ana'] / ds_obs['yield_trend']
 
-
     rank = xr.open_dataset('../data/result/corn_Prec_Tmax_sigma_bin_1981_2010_05deg.nc')
+    if climate_data == 'wfdei.gpcc':
+        ds_obs = ds_obs.isel(time=range(0,29))
+        rank = rank.isel(time=range(0,29))
 
     # prepare dataframe from obs
     t = xarray2dataframe(ds_obs['yield_ana_to_yield'], 'obs')
@@ -163,15 +223,15 @@ def agmip_to_csv():
 
     # Load model, covert and merge 
     for m in model_names[1::]:
-        ds = load_agmip_data(m, anomaly=True)
+        ds = load_agmip_data(m, anomaly=True, climate_data=climate_data)
         ds['yield_mai_ana_to_yield'] = ds['yield_mai_ana'] / ds['yield_mai_trend']
         t5 = xarray2dataframe(ds['yield_mai_ana_to_yield'], m)
 #        t5 = ds['yield_mai_ana_to_yield'].to_series().rename(m).to_frame().rename(m)
         t = t.merge(t5, on=['lat','lon','time'], how='left')
      
-    t.to_csv('../data/result/agmip_obs_yield_full.csv', index=None)
-    print('agmip converted to csv, file saved')
+    t.to_csv('../data/result/agmip_obs_yield_full_%s.csv'%climate_data, index=None)
+    print('agmip for %s converted to csv, file saved'%climate_data)
 
 if __name__ == '__main__':
-#    save_agmip_anomaly()
-    agmip_to_csv()
+#    save_agmip_anomaly(climate_data='wfdei.gpcc')
+    agmip_to_csv(climate_data='wfdei.gpcc')
